6b167de4de4810d232630bb2dcd8601ace2d801e
Dat4ever <nemodat8777@gmail.com>
2026-06-27 13:20:41 +0300

    nvidia.nix added

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
  imports =
   [
     ./hardware-configuration.nix
+    ./nvidia.nix
     ./stylix.nix
   ];
 
@@ -63,40 +64,6 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-  };
-
-  # Nvidia
-  boot.initrd.kernelModules = [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      nvidia-vaapi-driver
-      libva-vdpau-driver
-      libvdpau-va-gl
-    ];
-  };
-
-  environment.sessionVariables = {
-    NVD_BACKEND = "direct";
-  };
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.production;
-    open = true;
-    nvidiaSettings = true;
-    modesetting.enable = true;
-    prime = { 
-      intelBusId = "PCI:0@0:2:0";
-      nvidiaBusId = "PCI:1@0:0:0";
-      #sync = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-    };
   };
 
   # Run appimage
