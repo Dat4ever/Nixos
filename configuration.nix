d5712e97be0bd447aaf50216da1c2b917f6b7a5f
Dat4ever <nemodat8777@gmail.com>
2026-06-28 11:35:22 +0300

    font.nix debug

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
  imports =
   [
     ./hardware-configuration.nix
+    ./font.nix
     ./nvidia.nix
     ./stylix.nix
   ];
--- font.nix
+++ font.nix
@@ -1,13 +1,17 @@
-fonts.packages = with pkgs; [
-  (pkgs.stdenv.mkDerivation {
-    pname = "jetbrains-mono-config";
-    version = "1.0";
-    
-    src = ./config/etc/JetBrainsMono; 
+{ config, pkgs, ... }: 
 
-    installPhase = ''
-      mkdir -p $out/share/fonts/truetype
-      cp -r $src/*.{ttf,otf} $out/share/fonts/truetype/
-    '';
-  })
-];
+{
+  fonts.packages = with pkgs; [
+    (pkgs.stdenv.mkDerivation {
+      pname = "jetbrains-mono-config";
+      version = "1.0";
+    
+      src = ./config/etc/JetBrainsMono; 
+
+      installPhase = ''
+        mkdir -p $out/share/fonts/truetype
+        cp -r $src/*.{ttf,otf} $out/share/fonts/truetype/
+      '';
+    })
+  ];
+}
