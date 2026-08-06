33b830044077287181b3c740a46b52d60e952970
Dat4ever <dat4ever87@gmail.com>
2026-08-06 21:53:13 +0300

    lua_ls.lua root markers added

---
+++ hosts/datLOQ/home-config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,14 @@
+return {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".git", ".luarc.json", ".luarc.jsonc", ".luacheckrc", "stylua.toml", "selene.toml" },
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			workspace = {
+				checkThirdParty = false,
+				library = { vim.env.VIMRUNTIME },
+			},
+		},
+	},
+}
