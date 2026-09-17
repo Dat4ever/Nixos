8f8f374c4ce6b6877e8ef5f951d2e2b0b08e08f6
Dat4ever <dat4ever87@gmail.com>
2026-09-17 15:33:11 +0300

    changes 4

---
+++ hosts/datLOQ/home.d/desktop.nix
@@ -0,0 +1,106 @@
+{
+  xdg.enable = true;
+  dconf.enable = true;
+
+  # Xdg user dirs
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    # Xdg dirs locations
+    download = "$HOME/Downloads";
+    documents = "$HOME/Documents";
+    pictures = "$HOME/Media/Pictures";
+    videos = "$HOME/Media/Videos";
+    music = "$HOME/Media/Music";
+    desktop = "$HOME/Desktop";
+    templates = "$HOME/Desktop";
+    publicShare = "$HOME/Desktop";
+    extraConfig = {
+      PROJECTS = "$HOME/Projects";
+      BOOKS = "$HOME/Media/Books";
+      GAMES = "$HOME/Media/Games";
+    };
+  };
+
+  # Default terminal
+  xdg.configFile."xdg-terminals.list".text = "kitty.desktop\n";
+
+  # Default applications
+  # Usage: xdg-open <file> / xdg-mime query default <mime-type>
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      # Browser
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+      "application/pdf" = "org.pwmt.zathura.desktop";
+
+      # Images
+      "image/png" = [ "imv.desktop" "mpv.desktop" ];
+      "image/jpeg" = [ "imv.desktop" "mpv.desktop" ];
+      "image/gif" = [ "imv.desktop" "mpv.desktop" ];
+      "image/webp" = [ "imv.desktop" "mpv.desktop" ];
+      "image/svg+xml" = [ "imv.desktop" "mpv.desktop" ];
+      "image/bmp" = [ "imv.desktop" "mpv.desktop" ];
+      "image/tiff" = [ "imv.desktop" "mpv.desktop" ];
+
+      # Video
+      "video/mp4" = "mpv.desktop";
+      "video/x-matroska" = "mpv.desktop";
+      "video/webm" = "mpv.desktop";
+      "video/x-msvideo" = "mpv.desktop";
+
+      # Audio
+      "audio/mpeg" = "mpv.desktop";
+      "audio/flac" = "mpv.desktop";
+      "audio/ogg" = "mpv.desktop";
+      "audio/x-wav" = "mpv.desktop";
+
+      # Text
+      "text/plain" = "nvim.desktop";
+      "application/json" = "nvim.desktop";
+      "application/x-shellscript" = "nvim.desktop";
+      "text/markdown" = "nvim.desktop";
+
+      # Office
+      "application/msword" = "writer.desktop";
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "writer.desktop";
+      "application/rtf" = "writer.desktop";
+      "application/vnd.oasis.opendocument.text" = "writer.desktop";
+      "application/vnd.ms-excel" = "calc.desktop";
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "calc.desktop";
+      "application/vnd.oasis.opendocument.spreadsheet" = "calc.desktop";
+      "text/csv" = "calc.desktop";
+      "application/vnd.ms-powerpoint" = "impress.desktop";
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = "impress.desktop";
+      "application/vnd.oasis.opendocument.presentation" = "impress.desktop";
+
+      # Torrents
+      "application/x-bittorrent" = "org.qbittorrent.qBittorrent.desktop";
+      "x-scheme-handler/magnet" = "org.qbittorrent.qBittorrent.desktop";
+
+      # Archives
+      "application/zip" = "yazi.desktop";
+      "application/gzip" = "yazi.desktop";
+      "application/x-tar" = "yazi.desktop";
+      "application/x-7z-compressed" = "yazi.desktop";
+      "application/x-rar-compressed" = "yazi.desktop";
+      "application/vnd.rar" = "yazi.desktop";
+      "application/x-bzip2" = "yazi.desktop";
+
+      # Comic books
+      "application/vnd.comicbook+zip" = "org.pwmt.zathura.desktop";
+      "application/vnd.comicbook-rar" = "org.pwmt.zathura.desktop";
+
+      # Ebooks
+      "application/epub+zip" = "org.pwmt.zathura.desktop";
+
+      # Other
+      "application/xhtml+xml" = "librewolf.desktop";
+    };
+  };
+}
---
+++ hosts/datLOQ/home.d/dotconfig/nvim/init.lua
@@ -0,0 +1,66 @@
+-- Enable line numbers and relative numbers to jump to other lines on the screen using relative counts and cursorline for standing line
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Undo changes after exiting and reopening the file
+vim.opt.undofile = true
+
+-- 2 spaces for tab size, and tab key to insert spaces instead
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Copy paste integration
+vim.opt.clipboard = "unnamedplus"
+
+-- Leader key
+vim.g.mapleader = " " -- Space
+
+-- Ctrl + h/j/k/l and Ctrl + arrow keys to change focus
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-Left>", "<C-w>h")
+vim.keymap.set("n", "<C-Down>", "<C-w>j")
+vim.keymap.set("n", "<C-Up>", "<C-w>k")
+vim.keymap.set("n", "<C-Right>", "<C-w>l")
+
+-- <Leader> + e for open left netrw
+vim.keymap.set("n", "<leader>e", ":Lexplore<cr>", { silent = true })
+
+-- <Leader> + g for grep
+vim.keymap.set("n", "<leader>g", function()
+	vim.ui.input({ prompt = "Grep: " }, function(pattern)
+		if pattern then
+			vim.cmd("silent grep! " .. vim.fn.fnameescape(pattern))
+			vim.cmd("copen")
+		end
+	end)
+end, { silent = true })
+
+-- <Leader> + f for format
+vim.keymap.set("n", "<leader>f", function()
+	vim.lsp.buf.format({ async = true })
+end, { silent = true })
+
+-- <Leader> + s for find (search)
+vim.keymap.set("n", "<leader>s", ":find ", { silent = false })
+
+-- <Leader> + d for diagnostics
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.setqflist()
+  vim.cmd("copen")
+end, { silent = true })
+
+-- Other settings
+-- Theme (written by theme-switch, uses base16 colors)
+pcall(dofile, vim.fn.expand("~/.local/share/nvim/theme.lua"))
+
+require("lsp")
+require("terminalcolor")
+require("netrw")
+require("treesitter")
+require("statusline")
+require("find&grep")
